4044bd6dae4757d6f400fc72ecb18f4ae2e60bb9
sonal-uchiru <sonal123326@gmail.com>
2022-01-03 13:52:56 +0530

    feature page completed successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeaturespageComponent } from './staticcomponents/featurespage/featurespage.component';
+import { GetstartedComponent } from './buttons/getstarted/getstarted.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturespageComponent,
+    GetstartedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/staticcomponents/featurespage/featurespage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featurespage',
+  templateUrl: './featurespage.component.html',
+  styleUrls: ['./featurespage.component.css']
+})
+export class FeaturespageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
