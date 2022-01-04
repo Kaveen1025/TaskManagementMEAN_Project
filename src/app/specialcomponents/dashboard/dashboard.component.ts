574202484d928ce69204bfb0c65357f56dc05852
sonal-uchiru <sonal123326@gmail.com>
2022-01-04 21:02:11 +0530

    dynamic dashboard completed without responsive interface

--- src/app/specialcomponents/dashboard/dashboard.component.ts
+++ src/app/specialcomponents/dashboard/dashboard.component.ts
@@ -6,10 +6,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  imagePath: String
+  imageURL: String
+  public now: Date =  new Date()
+  Time:String
+  Date:String
+  todayDataTime:String
+  constructor() {
+    this.imagePath = "./assets/images/Dynamic%20Image%20Collection/"
+    this.imageURL = ""
+    this.todayDataTime = ""
+    this.Time = ""
+    this.Date = ""
+    setInterval(() => {
+       this.now = new Date();
+       this.todayDataTime = String(this.now)
+       this.Time = this.todayDataTime.substring(16, 21)
+       this.Date = this.todayDataTime.substring(0,16)
+       this.dynamicWallpaper(this.todayDataTime.substring(16,18))
+      //this.now = formatDate(new Date(), 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0530')
+    }, 1);
 
-  constructor() { }
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.imageURL = this.imagePath + "Time_4_6.png";
+  }
+
+  dynamicWallpaper(timePrefix:String):void{
+    if( Number(timePrefix) >= 4 && Number(timePrefix) < 6){
+      this.imageURL = this.imagePath + "Time_4_6.png"
+    }else if( Number(timePrefix) >= 6 && Number(timePrefix) < 8){
+      this.imageURL = this.imagePath + "Time_6_8.png"
+    }else if( Number(timePrefix) >= 8 && Number(timePrefix) < 10){
+      this.imageURL = this.imagePath + "Time_8_10.png"
+    }else if( Number(timePrefix) >= 10 && Number(timePrefix) < 12){
+      this.imageURL = this.imagePath + "Time_10_12.png"
+    }else if( Number(timePrefix) >= 12 && Number(timePrefix) < 14){
+      this.imageURL = this.imagePath + "Time_12_14.png"
+    }else if( Number(timePrefix) >= 14 && Number(timePrefix) < 16){
+      this.imageURL = this.imagePath + "Time_14_16.png"
+    }else if( Number(timePrefix) >= 16 && Number(timePrefix) < 18){
+      this.imageURL = this.imagePath + "Time_16_18.png"
+    }else if( Number(timePrefix) >= 18 && Number(timePrefix) < 20){
+      this.imageURL = this.imagePath + "Time_18_20.png"
+    }else if( Number(timePrefix) >= 20 && Number(timePrefix) < 22){
+      this.imageURL = this.imagePath + "Time_20_22.png"
+    }else if( Number(timePrefix) >= 22 && Number(timePrefix) < 0){
+      this.imageURL = this.imagePath + "Time_22_12.png"
+    }else if( Number(timePrefix) >= 0 && Number(timePrefix) < 4){
+      this.imageURL = this.imagePath + "Time_12_4.png"
+    }
+  }
+  changeImage(number: number) {
+    switch (number) {
+      case 1:
+        this.imageURL = this.imagePath + "Time_4_6.png"
+        break;
+        case 2:
+          this.imageURL = this.imagePath + "Time_6_8.png"
+        break;
+      case 3:
+        this.imageURL = this.imagePath + "Time_8_10.png"
+        break;
+      case 4:
+        this.imageURL = this.imagePath + "Time_10_12.png"
+        break;
+      case 5:
+        this.imageURL = this.imagePath + "Time_12_14.png"
+        break;
+      case 6:
+        this.imageURL = this.imagePath + "Time_14_16.png"
+        break;
+      case 7:
+        this.imageURL = this.imagePath + "Time_16_18.png"
+        break;
+      case 8:
+        this.imageURL = this.imagePath + "Time_18_20.png"
+        break;
+      case 9:
+        this.imageURL = this.imagePath + "Time_20_22.png"
+        break;
+      case 10:
+        this.imageURL = this.imagePath + "Time_22_12.png"
+        break;
+      case 11:
+        this.imageURL = this.imagePath + "Time_12_4.png"
+        break;
+    }
+  }
 }
