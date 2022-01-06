0de78cf72609e1bd49121d82aa227b64d1fbf914
sonal-uchiru <sonal123326@gmail.com>
2022-01-06 11:39:49 +0530

    guard routes incomplete

--- src/app/security/guarded-routes-guard.service.ts
+++ src/app/security/guarded-routes-guard.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router} from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardedRoutesGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor() {
+  }
+
+  canActivate(): boolean {
+    return false;
   }
 
 }
