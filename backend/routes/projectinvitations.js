bce137c739d41eb7e48bbf1edc65696464536528
BamunugeDR <drbamunuge666@gmail.com>
2022-01-09 16:24:18 +0530

    User and Projects Routes(Work in progress 4)

--- backend/routes/projectinvitations.js
+++ backend/routes/projectinvitations.js
@@ -3,10 +3,11 @@
 //Imports
 const router = require("express").Router();
 let ProjectInvitation = require('../models/projectInvitations');
+const WorkspaceInvitaion = require("../models/workspaceInvitations");
 
 
 //Create new ProjectInvitation
-//URL -->http://localhost:8070/
+//URL -->http://localhost:8070/projectinv/add
 router.route("/add").post((req, res) => {
   const {
     projectID,
@@ -33,6 +34,59 @@ router.route("/add").post((req, res) => {
 });
 
 
+//Fetch Project data according to user ID and Project ID
+//URL -->http://localhost:8070/projectinv/get/:userID
+router.route('/get/:userID').get(async (req, res) => {
+  let UserID = req.params.userID;
 
+  ProjectInvitation.find({invitedUser:UserID}).then((projects) => {
+    res.json(projects);
+  }).catch((err) => {
+    console.log(err.message);
+    res
+      .status(500)
+      .send({ status: "Error with retrieving  Project", error: err.message });
+  });
+
+})
+
+
+//Delete Project Invitation
+//URL -->http://localhost:8070/projectinv/delete/:id
+router.route("/delete/:id").delete(async (req, res) => {
+  let ProjectInvitaionID = req.params.id;
+
+  await ProjectInvitation.findByIdAndDelete(ProjectInvitaionID)
+    .then(() => {
+      res.status(200).send({status: "Project Invitation Successfully Deleted"});
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({status: "Error while deleting workspace Invitation", error: err.message});
+    });
+});
+
+//Delete ProjectInvitaion using workspace ID and userID
+//URL -->http://localhost:8070/projectinv/delete/:projectID/:userID
+router.route("/delete/:projectID/:userID").delete(async (req, res) => {
+  let UserID = req.params.userID;
+  let ProjectID = req.params.projectID;
+
+
+  await ProjectInvitation.findOneAndDelete({$and: [{projectID: ProjectID}, {invitedUser: UserID}]})
+    .then((req2, res2) => {
+      res.status(200).send({status: "Project Invitation deleted"});
+    }).catch((err1) => {
+      console.log(err1.message);
+
+      res.status(500).send({status: "UserID or ProjectID is invalid", error: err1.message});
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({status: "Error while deleting Project Invitation", error: err.message});
+    });
+});
 
 module.exports = router;
