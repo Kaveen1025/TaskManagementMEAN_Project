c7fe58d8868daf67b8b6be93fc4d7ecda8acad46
BamunugeDR <drbamunuge666@gmail.com>
2022-01-10 17:43:30 +0530

    User and Projects Routes(Done)

---
+++ src/app/cards/generalnoifications/generalnoifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generalnoifications',
+  templateUrl: './generalnoifications.component.html',
+  styleUrls: ['./generalnoifications.component.css']
+})
+export class GeneralnoificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
