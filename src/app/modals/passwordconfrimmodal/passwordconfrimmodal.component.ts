9cfafde0296fad672936b77612b3a00d9c90219e
sonal-uchiru <sonal123326@gmail.com>
2022-01-13 20:49:16 +0530

    delete account 80% completed

--- src/app/modals/passwordconfrimmodal/passwordconfrimmodal.component.ts
+++ src/app/modals/passwordconfrimmodal/passwordconfrimmodal.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {FormControl} from "@angular/forms";
+import {UserService} from "../../services/user.service";
+// @ts-ignore
+import * as bcrypt from 'bcryptjs';
+
 
 @Component({
   selector: 'app-passwordconfrimmodal',
@@ -6,10 +12,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./passwordconfrimmodal.component.css']
 })
 export class PasswordconfrimmodalComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('content') private content: TemplateRef<any> | undefined;
+  currentPassword = new FormControl('');
+  typeInput1:any
+  eyeImageUrl1:any
+  userID:String
+  User:any
+  UserService:UserService
+  constructor(UserService:UserService,private modalService: NgbModal) {
+    this.typeInput1 = "password"
+    this.eyeImageUrl1 = "./assets/images/images%20Used%20in%20Project%20Management%20UI%20Design/visibility.png"
+    this.UserService = UserService
+    this.userID = "61d59e7999dc1f31177898ba"
+  }
 
   ngOnInit(): void {
+      this.getUser()
+  }
+
+
+  getUser(){
+    this.UserService.getUser(this.userID).subscribe({
+      next:value=>
+      {
+        this.User = value
+      }
+      ,
+      error:error => {
+        console.log(error)
+      }
+    } )
+  }
+  keyDownHandler(event: any) {
+    if (event.which === 32)
+      event.preventDefault();
+  }
+
+
+  saveDetails() {
+
+    console.log(this.currentPassword.value)
+    console.log(this.User)
+
+    if(bcrypt.compareSync(this.currentPassword.value,this.User.Password)){
+      console.log("true")
+    }else {
+      console.log("false")
+    }
+  }
+
+  openModal(){
+    this.modalService.open(this.content, { centered: true });
+  }
+  closeModal() {
+    this.modalService.dismissAll(this.content);
+  }
+
+
+  toggleEye() {
+    if(this.typeInput1 == "text"){
+      this.typeInput1 = "password"
+      this.eyeImageUrl1 = "./assets/images/images%20Used%20in%20Project%20Management%20UI%20Design/visibility.png"
+    }else{
+      this.typeInput1 = "text"
+      this.eyeImageUrl1 = "./assets/images/images%20Used%20in%20Project%20Management%20UI%20Design/open.png"
+    }
+
   }
 
 }
