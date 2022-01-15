88dfa652ffe62288c369fd4daedd60f4eb9d39b1
kavee <IT20185770@my.sliit.lk>
2022-01-15 10:58:07 +0530

    signup pages services

--- src/app/components/signuppage/signuppage.component.ts
+++ src/app/components/signuppage/signuppage.component.ts
@@ -9,9 +9,50 @@ import {FormGroup, FormControl, Validators, NgForm} from "@angular/forms";
 })
 export class SignuppageComponent implements OnInit {
 
-  constructor() { }
+  Username = new FormControl('');
+  Email = new FormControl('');
+  FirstName = new FormControl('');
+  LastName = new FormControl('');
+  Password = new FormControl('');
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
 
   }
+
+  addUser(contactForm: NgForm){
+
+    let object2 = {
+
+      Username: this.Username.value,
+      Email: this.Email.value,
+      FirstName: this.FirstName.value,
+      LastName: this.LastName.value,
+      Password: this.Password.value
+
+    }
+
+    console.log(object2)
+    this.userService.createUser(object2).subscribe((post: any)=> {
+
+      this.Username.setValue("");
+      this.Email.setValue("");
+      this.FirstName.setValue("");
+      this.LastName.setValue("");
+      this.Password.setValue("");
+
+
+      alert("Success");
+      // location.reload();
+      console.log("Success");
+      // console.log(this.projectObject[0]);
+    }, error => {
+      console.log(error);
+    });
+
+
+  }
+
+
 }
