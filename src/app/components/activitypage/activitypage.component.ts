e95e36675b217823463dddbf3db246cc9156b799
TharinduDeshan <tharindudeshan50@gmail.com>
2022-01-20 18:45:39 +0530

    activity design ongoing

---
+++ src/app/components/activitypage/activitypage.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activitypage',
+  templateUrl: './activitypage.component.html',
+  styleUrls: ['./activitypage.component.css']
+})
+export class ActivitypageComponent implements OnInit {
+
+
+  workspaceID = "61d448976fc2f6cc55b25ca5";
+  // workspaceID: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
