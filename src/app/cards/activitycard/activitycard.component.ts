d1a7f0b5e4f70e19b679d273f37afb034d02aaa9
sonal-uchiru <sonal123326@gmail.com>
2022-01-22 23:43:16 +0530

    new

--- src/app/cards/activitycard/activitycard.component.ts
+++ src/app/cards/activitycard/activitycard.component.ts
@@ -9,6 +9,7 @@ export class ActivitycardComponent implements OnInit {
 
   @Input() cardTitle: string | undefined
   @Input() collapseID: string | undefined
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit(): void {
